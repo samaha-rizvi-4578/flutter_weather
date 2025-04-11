8702b5a22130a518076befa6428d8498b5cad93a
SamahaRizvi <k214578@nu.edu.pk>
2025-04-11 07:49:10 +0500

    Add JSON serialization support to Location and Weather models

--- packages/open_meteo_api/lib/src/models/location.dart
+++ packages/open_meteo_api/lib/src/models/location.dart
@@ -1,6 +1,10 @@
 // store data returned by location api
 // https://geocoding-api.open-meteo.com/v1/search?name=karachi&count=1
+import 'package:json_annotation/json_annotation.dart';
 
+part 'location.g.dart';
+
+@JsonSerializable()
 class Location {
   const Location({
     required this.id,
@@ -8,6 +12,10 @@ class Location {
     required this.latitude,
     required this.longitude,
   });
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
+
   final int id;
   final String name;
   final double latitude;
--- packages/open_meteo_api/lib/src/models/weather.dart
+++ packages/open_meteo_api/lib/src/models/weather.dart
@@ -1,10 +1,19 @@
 // store data returnd by waeterh api
 // https://api.open-meteo.com/v1/forecast?latitude=24.8608&longitude=67.0104&current_weather=true
+import 'package:json_annotation/json_annotation.dart';
+part 'weather.g.dart';
+
+@JsonSerializable()
 class Weather {
   const Weather ({
     required this.temperature, 
     required this.weatherCode,
   });
+
+  factory Weather.fromJson(Map<String, dynamic> json) =>
+      _$WeatherFromJson(json);
+
   final double temperature;
-  final int weatherCode;
+  @JsonKey(name: 'weathercode')
+  final double weatherCode;
 }
