ade6b5c6cde886f6cd9fd8f313911cf2b14268aa
SamahaRizvi <k214578@nu.edu.pk>
2025-04-11 09:11:05 +0500

    implement weather fetching logic

--- packages/weather_repository/lib/weather_repository.dart
+++ packages/weather_repository/lib/weather_repository.dart
@@ -3,3 +3,6 @@
 library weather_repository;
 
 export 'src/models/models.dart';
+export 'src/weather_repository.dart';
+
+//  The goal of the WeatherRepository is to separate the “what” from the “how” — in other words, we want to have a way to fetch weather for a given city, but don’t care about how or where that data is coming from.
